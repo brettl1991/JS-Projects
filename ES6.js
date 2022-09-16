8f360026bba0a6b79265b4cf476af0821d51bd85
brettl1991 <brettl1991@gmail.com>
2022-09-16 11:34:31 +0100

    Const, let, arrow func

--- ES6.js
+++ ES6.js
@@ -1,1 +1,31 @@
 "use strict";
+//const and let
+let age = 20; //Can be reassigned
+console.log(age);
+age = age + 5;
+console.log(age);
+
+const name = "John"; //Can not be reassigned
+// name = "Jane";
+
+//Arrow functions
+
+//normal func
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(2, 2);
+
+//arrow
+const add1 = (a, b) => {
+  console.log(a + b);
+};
+
+add(2, 3);
+
+//example
+const array = [1, 2, 3];
+array.map((n) => {
+  console.log(n);
+});
